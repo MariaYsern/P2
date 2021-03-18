de8abc814d952e159cf7456f6edeeff387906a1b
Mireia Ambrós Vivancos <mireia.ambros@estudiantat.upc.edu>
2021-03-18 11:07:09 +0100

    meson.build changed2

--- src/pav_analysis.h
+++ src/pav_analysis.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef PAV_ANALYSIS_H
 #define PAV_ANALYSIS_H
 
@@ -6,13 +5,4 @@ float compute_power(const float *x, unsigned int N);
 float compute_am(const float *x, unsigned int N);
 float compute_zcr(const float *x, unsigned int N, float fm);
 
-=======
-#ifndef PAV_ANALYSIS_H
-#define PAV_ANALYSIS_H
-
-float compute_power(const float *x, unsigned int N);
-float compute_am(const float *x, unsigned int N);
-float compute_zcr(const float *x, unsigned int N, float fm);
-
->>>>>>> 495d76a0abb5e30e5971611360b668e374fd82d5
 #endif /* PAV_ANALYSIS_H */
